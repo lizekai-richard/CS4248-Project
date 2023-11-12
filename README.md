b071098d6fc316c3408e074fd08e108751af01b9
lizekai-richard <lzknus@gmail.com>
2023-11-12 15:47:23 +0800

    Update code

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # CS4248-Project
+
+## Train
+
+To train the MCQ model, run
+
+```bash
+bash scripts/train.sh
+```
+
+You can modify configurations in the train.sh
+
+```bash
+ALBERT_PATH="twmkn9/albert-base-v2-squad2"
+DEBERTA_PATH="deepset/deberta-v3-base-squad2"
+ELECTRA_PATH="deepset/electra-base-squad2"
+# change to yours
+TRAIN_DATA_PATH="YOUR DATA PATH"
+DEV_DATA_PATH="YOUR DATA PATH"
+MCQ_MODEL_PATH="YOUR MODEL PATH"
+OUTPUT_PATH="YOUR MODEL CHECKPOINT PATH"
+
+python3 run.py \
+--do_train True \
+--output_path $OUTPUT_PATH \
+--train_data_path $TRAIN_DATA_PATH \
+--dev_data_path $DEV_DATA_PATH \
+--albert_path $ALBERT_PATH \
+--deberta_path $DEBERTA_PATH \
+--electra_path $ELECTRA_PATH \
+--mcq_model_path $MCQ_MODEL_PATH \
+# you can change the following training settings
+--max_length 512 \
+--batch_size 32 \
+--eval_steps 5000 \
+--save_steps 5000
+```
+
+## Evaluate
+
+To do evaluation, you can run
+
+```bash
+bash scripts/eval.sh
+```
+
+Similarly, you should modify the script before you run
+
+```bash
+ALBERT_PATH="twmkn9/albert-base-v2-squad2"
+DEBERTA_PATH="deepset/deberta-v3-base-squad2"
+ELECTRA_PATH="deepset/electra-base-squad2"
+# change to yours
+TRAIN_DATA_PATH="YOUR DATA PATH"
+DEV_DATA_PATH="YOUR DATA PATH"
+MCQ_MODEL_PATH="YOUR TRAINED MODEL CHECKPOINT"
+SAVE_PATH="YOUR PREDICTIONS SAVE PATH"
+
+python3 run.py \
+--do_train False \
+--save_path $SAVE_PATH \
+--train_data_path $TRAIN_DATA_PATH \
+--dev_data_path $DEV_DATA_PATH \
+--albert_path $ALBERT_PATH \
+--deberta_path $DEBERTA_PATH \
+--electra_path $ELECTRA_PATH \
+--mcq_model_path $MCQ_MODEL_PATH
+```
+
--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from datasets import load_dataset
 from dataset import SQuADDataset, MCQDataset, DataCollatorForMultipleChoice, preprocess_dataset_for_training_qna
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering, AutoModelForMultipleChoice, TrainingArguments, \
-    Trainer, pipeline
+    Trainer
 
 
 def reformat_dataset(data):
@@ -60,6 +60,7 @@ def single_model_generate_predictions(tokenizers, models, data_loader):
             model = models[model_name]
 
             input_ids, attention_mask = batch[model]['input_ids'], batch[model]['attention_mask']
+            input_ids, attention_mask = input_ids.cuda(), attention_mask.cuda()
             # ans_starts, ans_ends = batch[model]['start_positions'], batch[model]['end_positions']
 
             outputs = model(input_ids, attention_mask)
@@ -68,7 +69,7 @@ def single_model_generate_predictions(tokenizers, models, data_loader):
             pred_ends = outputs.end_logits.argmax()
 
             for i, (s, e) in enumerate(zip(pred_starts, pred_ends)):
-                pred_answer = tokenizer.decode(input_ids[i][s: e + 1])
+                pred_answer = tokenizer.decode(input_ids[i][s: e + 1], skip_special_tokens=True)
                 predictions[model_name].append(pred_answer)
 
     return predictions
@@ -80,6 +81,7 @@ def ensemble_model_generate_predictions(model, data_loader, dataset):
     prediction_labels = []
     for step, data in enumerate(data_loader, 0):
         input_ids, attention_mask = data['input_ids'], data['attention_mask']
+        input_ids, attention_mask = input_ids.cuda(), attention_mask.cuda()
 
         outputs = model(input_ids, attention_mask)
         prediction_labels.extend(outputs.logits.argmax(axis=1).tolist())
@@ -91,6 +93,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--do_train", type=bool, default=True)
     parser.add_argument("--save_path", type=str, default="/path/to/save")
+    parser.add_argument("--output_path", type=str, default="/path/to/output")
     parser.add_argument("--train_data_path", type=str, default="/path/to/train/data")
     parser.add_argument("--dev_data_path", type=str, default="/path/to/dev/data")
     parser.add_argument("--deberta_path", type=str, default="/path/to/bert")
@@ -99,6 +102,9 @@ if __name__ == '__main__':
     parser.add_argument("--mcq_model_path", type=str, default="/path/to/mcq/model")
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--max_length", type=int, default=512)
+    parser.add_argument("--learning_rate", type=float, default=5e-5)
+    parser.add_argument("--eval_steps", type=int, default=5000)
+    parser.add_argument("--save_steps", type=int, default=5000)
     args = parser.parse_args()
 
     tokenizers = {
@@ -131,24 +137,25 @@ if __name__ == '__main__':
     mcq_tokenizer = AutoTokenizer.from_pretrained(args.mcq_model_path)
     mcq_model = AutoModelForMultipleChoice.from_pretrained(args.mcq_model_path)
 
-    print("Generating single model predictions on the train dataset...")
-    single_model_predictions_on_train_data = single_model_generate_predictions(tokenizers, models,
-                                                                               train_loader)
-    print("Generating single model predictions on the dev dataset...")
-    single_model_predictions_on_dev_data = single_model_generate_predictions(tokenizers, models,
-                                                                             dev_loader)
-
     if args.do_train:
         print("For convenience of format consistency, we use dataset on huggingface to do the training. "
               "But the evaluation is conducted on the given dataset")
         squad = load_dataset("squad")
+
+        print("Generating single model predictions on the train dataset...")
+        single_model_predictions_on_train_data = single_model_generate_predictions(tokenizers, models,
+                                                                                   train_loader)
         train_mcq_ds = preprocess_dataset_for_training_qna(squad['train'], single_model_predictions_on_train_data,
                                                            mcq_tokenizer)
+
+        print("Generating single model predictions on the dev dataset...")
+        single_model_predictions_on_dev_data = single_model_generate_predictions(tokenizers, models,
+                                                                                 dev_loader)
         dev_mcq_ds = preprocess_dataset_for_training_qna(squad['validation'], single_model_predictions_on_dev_data,
                                                          mcq_tokenizer)
 
         training_args = TrainingArguments(
-            output_dir="squad_qna_model",
+            output_dir=args.output_path,
             evaluation_strategy="steps",
             eval_steps=5000,
             save_strategy="steps",
@@ -174,6 +181,9 @@ if __name__ == '__main__':
 
         trainer.train()
     else:
+        print("Generating single model predictions on the dev dataset...")
+        single_model_predictions_on_dev_data = single_model_generate_predictions(tokenizers, models,
+                                                                                 dev_loader)
         eval_mcq_data = add_predictions_to_dataset(dev_data, single_model_predictions_on_dev_data)
 
         # Step 3: create the mcq dataset for prediction
