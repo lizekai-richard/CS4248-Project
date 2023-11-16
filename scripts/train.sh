92740f1f095de759800ecf15e8869b3cbbea692f
lizekai-richard <lzknus@gmail.com>
2023-11-16 21:42:01 +0800

    Update

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,7 @@ TRAIN_DATA_PATH="./data/train-v1.1.json"
 DEV_DATA_PATH="./data/dev-v1.1.json"
 ALBERT_PATH="twmkn9/albert-base-v2-squad2"
 DEBERTA_PATH="deepset/deberta-v3-base-squad2"
-ELECTRA_PATH="deepset/electra-base-squad2"
+ELECTRA_PATH="valhalla/electra-base-discriminator-finetuned_squadv1"
 MCQ_MODEL_PATH="distilbert-base-uncased"
 OUTPUT_PATH="ensemble_mcq_model"
 
